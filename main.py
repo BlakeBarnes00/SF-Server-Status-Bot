ea11c7fabdd950174963f02916f60e634b11ebe2
Ellis Barnes <ellisbarnes00@gmail.com>
2021-12-11 21:38:25 -0500

    Bot done to what the client wanted.
    Finished the basic structure that the client had wanted, waiting to see
    if they want more or any other tweaks.

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
+import discord, asyncio
+from discord import channel
+from discord import client
+
 import requests, json
+from commands import *
 
-def get_server_info(json_data):
-	print("Server Status: " + json_data["data"]["serverStatus"])
-	print("Server Time: " + json_data["data"]["serverTime"])
-
-def get_next_occurences(json_data):
-	next_occurances = json_data["data"]["nextOccurences"]
-	print("Next Occurences")
-	print("Guild Siege: " + next_occurances["guildSiege"])
-	print("Capture the Flag: " + next_occurances["captureTheFlag"])
-	print("Team Deathmatch: " + next_occurances["teamDeathmatch"])
-	print("Dodge the Attack: " + next_occurances["dodgeTheAttack"])
-	print("Monster Invasion: " + next_occurances["monsterInvasion"])
-	print("Dungeon Energy: " + next_occurances["dungeonEnergy"])
-	print("Daily Mission: " + next_occurances["dailyMission"])
-	print("Weekly Mission: " + next_occurances["weeklyMission"])
+TOKEN = "" 
+with open("token.txt") as f:
+	TOKEN = f.readline()
 
 
-url = "https://stellarflyff.com/server-status"
-req = requests.get(url)
+class Client(discord.Client):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 
-if req.status_code == 200:
-	content = json.loads(req.content)
-	get_server_info(content)
-	get_next_occurences(content)
+		# create the background task and run it
+		self.bg_task = self.loop.create_task(self.print_info())
+
+	# Prints into console on start
+	async def on_ready(self):
+		print("Logged in as")
+		print(self.user.name)
+		print(self.user.id)
+		print("----------------")
+	
+	# Prints the info
+	async def print_info(self):
+		await self.wait_until_ready()
+		counter = 0
+		channel = self.get_channel(919390138868584489)
+		while not self.is_closed():
+			counter += 1
+			await channel.send(get_server_info(content) + '\n' + get_next_occurences(content))
+			await asyncio.sleep(60*5) # runs every 5 minutes
+
+req = requests.get(url="https://stellarflyff.com/server-status")
+content = json.loads(req.content) # the json content from the website
+
+
+client = Client()
+client.run(TOKEN)
