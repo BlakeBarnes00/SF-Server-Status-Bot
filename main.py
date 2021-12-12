1d0b45f8d36c3bbebd3b8323bdeafbbdca8e85e4
Ellis Barnes <ellisbarnes00@gmail.com>
2021-12-11 19:36:44 -0500

    Parsed the next event times

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ url = "https://stellarflyff.com/server-status"
 
 req = requests.get(url)
 if req.status_code == 200:
-	data = json.loads(req.content)
+	c = json.loads(req.content)
+	print("Server Status: " + c["data"]["serverStatus"])
+	print("Server Time: " + c["data"]["serverTime"])
 
-	for section in ["data"]:
-		for sub_section in ["serverStatus", "serverTime"]:
-			print("Server Status: " + data[section][sub_section])
+	#print(c["data"]["nextOccurences"])
+	
+	next_occurances = c["data"]["nextOccurences"]
+	print("\nNext Occurences")
+	print("Guild Siege: " + next_occurances["guildSiege"])
+	print("Capture the Flag: " + next_occurances["captureTheFlag"])
+	print("Team Deathmatch: " + next_occurances["teamDeathmatch"])
+	print("Dodge the Attack: " + next_occurances["dodgeTheAttack"])
+	print("Monster Invasion: " + next_occurances["monsterInvasion"])
+	print("Dungeon Energy: " + next_occurances["dungeonEnergy"])
+	print("Daily Mission: " + next_occurances["dailyMission"])
+	print("Weekly Mission: " + next_occurances["weeklyMission"])
+
